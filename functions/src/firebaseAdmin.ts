7e3afbdba251731cfd6de843a2d698ebce15823d
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-05-31 15:03:03 +0200

    refactored and simplified admin sdk authentication on the server

--- functions/src/firebaseAdmin.ts
+++ functions/src/firebaseAdmin.ts
@@ -1,15 +1,11 @@
 import * as admin from "firebase-admin";
-import { getApps } from "firebase-admin/app";
 import serviceAccountJson from "../serviceAccountKey.json";
 
 const serviceAccount = serviceAccountJson as admin.ServiceAccount;
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
 
-if (!getApps().length) {
-	admin.initializeApp({
-		credential: admin.credential.cert(serviceAccount)
-	});
-}
-
-const adminDb = admin.firestore(); 
+const adminDb = admin.firestore();
 adminDb.settings({ ignoreUndefinedProperties: true });
 export { adminDb };
