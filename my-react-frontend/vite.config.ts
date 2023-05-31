e2f9663300b047549739ee6fa92aa4302a633923
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-05-31 15:33:21 +0200

    - removed react refresh package from vite config

--- my-react-frontend/vite.config.ts
+++ my-react-frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import { splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	root: "./src",
-	plugins: [react(), reactRefresh(), splitVendorChunkPlugin()],
+	plugins: [react(), splitVendorChunkPlugin()],
 	build: {
 		outDir: '../public'
 	  }
