4a3a0247df7586c2573d94660dbdc2811a3135b8
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-05-31 15:02:29 +0200

    refactored sccheduled functions to allow a custom timeout of 500 seconds

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import { adminDb } from "./firebaseAdmin";
 
 //Import firebase functions
 import { logger } from "firebase-functions";
+import * as functions from "firebase-functions";
 
 import cleanupOldEvents from "./cleanup";
 import updateDatabase from "./update";
@@ -11,28 +12,43 @@ import { scrapeMeetup } from "./my-scraper/scrapers/ScrapeMeetup";
 import { scrapeEventbrite } from "./my-scraper/scrapers/ScrapeEventbrite";
 
 import Event from "./types/Event";
-import { onSchedule } from "firebase-functions/v2/scheduler";
+
+const runtimeOpts = {
+	timeoutSeconds: 300,
+};
 
 const meetupUrl =
 	"https://www.meetup.com/find/?location=de--M%C3%BCnchen&source=EVENTS&sortField=RELEVANCE&eventType=inPerson&categoryId=546 ";
 const eventbriteUrl =
 	"https://www.eventbrite.com/d/germany--m%C3%BCnchen/free--science-and-tech--events/?ang=en";
 
-exports.cleanupEvents = onSchedule("every day 00:00", async () => {
-	await cleanupOldEvents(adminDb);
-	logger.log("Event cleanup finished");
-});
+exports.cleanupEvents = functions
+	.runWith(runtimeOpts)
+	.pubsub.schedule("0 0 * * *")
+	.timeZone("Europe/Berlin")
+	.onRun(async () => {
+		await cleanupOldEvents(adminDb);
+		logger.log("Event cleanup finished");
+	});
 
-exports.meetupScraper = onSchedule("every day 01:00", async () => {
-	const events: Event[] = await runScraper(meetupUrl, scrapeMeetup);
-	await updateDatabase(events, adminDb);
-	logger.log("Event meetup data update finished");
-});
+exports.meetupScraper = functions
+	.runWith(runtimeOpts)
+	.pubsub.schedule("0 1 * * *")
+	.timeZone("Europe/Berlin")
+	.onRun(async () => {
+		const events: Event[] = await runScraper(meetupUrl, scrapeMeetup);
+		await updateDatabase(events, adminDb);
+		logger.log("Event meetup data update finished");
+	});
 
-exports.eventbriteScraper = onSchedule("every day 02:00", async () => {
-	const events = await runScraper(eventbriteUrl, scrapeEventbrite);
-	await updateDatabase(events, adminDb);
-	logger.log("Event eventbrite data update finished");
-});
+exports.eventbriteScraper = functions
+	.runWith(runtimeOpts)
+	.pubsub.schedule("0 2 * * *")
+	.timeZone("Europe/Berlin")
+	.onRun(async () => {
+		const events = await runScraper(eventbriteUrl, scrapeEventbrite);
+		await updateDatabase(events, adminDb);
+		logger.log("Event eventbrite data update finished");
+	});
 
 export { meetupUrl, eventbriteUrl, adminDb };
