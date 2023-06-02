aa1891aca8a1eb920bfe2bca7123e0c54bf87132
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-06-02 16:03:55 +0200

    - updated packages - make header appear thinner on loading

--- my-react-frontend/src/components/Header.tsx
+++ my-react-frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { FiCpu } from "react-icons/fi"; //This is an example of a tech icon you 
 
 const HeaderComponent = () => {
 	return (
-		<nav className="flex-1 flex items-center lg:justify-center bg-neutral2 text-neutral1 shadow-lg contain">
+		<nav className="flex items-center lg:justify-center bg-neutral2 text-neutral1 shadow-lg contain">
 				<button className="inline-flex items-center m-2 p-3 rounded-md text-gray-400 hover:text-white hover:bg-neutral2 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white contain">
 					<FiCpu className="block h-8 w-auto" color="complementary" />
 				</button>
