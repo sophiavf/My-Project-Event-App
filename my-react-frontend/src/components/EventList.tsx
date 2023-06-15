5f6839e0f7d66d2518b5dcc87a091ac301bcd98b
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-06-15 18:35:12 +0200

    tried to implement page pagination but unfortunately it is not possible

--- my-react-frontend/src/components/EventList.tsx
+++ my-react-frontend/src/components/EventList.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import {
 	collection,
+	doc,
+	getDoc,
 	getDocs,
 	query as queryF,
 	orderBy,
@@ -25,62 +27,74 @@ function EventList() {
 	const [totalPages, setTotalPages] = useState<number>(1);
 	const { pageNum } = useParams(); //The useParams hook returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>. Child routes inherit all params from their parent routes.
 	const navigate = useNavigate();
-	const page = pageNum ? Number(pageNum) : 1;
+	let page = Number(pageNum);
 	const eventsPerPage: number = 20;
-
-	// Creates state for page ends, which is retrieved depending on which page the user is on so they can navigate back and forth between pages
-	const [pageEnds, setPageEnds] = useState<Map<number, any>>(new Map());
 
 	useEffect(() => {
 		const fetchEvents = async () => {
 			// by default, on load page is set equal to 1 so the page on load is the 1st page
-			if (page === 1 && !pageNum) {
-				navigate("/page/1");
-			}
+			// Redirect to page 1 if pageNum is not set
+
 			try {
+				if (!page) {
+					navigate("/page/1");
+					page = 1;
+				}
 				const eventsRef = collection(db, "events");
+				// Calculate the total number of pages const totalEvents = await getCountFromServer(eventsRef);
 				const totalEvents = await getCountFromServer(eventsRef);
 				const totalEventsCount = totalEvents.data().count;
 				setTotalPages(Math.ceil(totalEventsCount / eventsPerPage));
 
-				let q;
+				let eventQuery;
+
 				if (page === 1) {
-					q = queryF(eventsRef, orderBy("dateTime"), limit(eventsPerPage));
-					//if there is a record in the pageEnds state for the previous page, then the last event on that page is used as a reference to fetch events after it for the next. It holds key-value pairs where the keys are of type number (pages) and the values are of any type (any), which returns the las document of a specific page to be used in the cursor pagination for firestore.
-				} else if (pageEnds.has(page - 1)) {
-					q = queryF(
+					// For the first page, fetch the first 20 events
+					eventQuery = queryF(
 						eventsRef,
 						orderBy("dateTime"),
-						startAfter(pageEnds.get(page - 1)),
 						limit(eventsPerPage)
 					);
 				} else {
-					return;
-				}
-				const eventsSnapshot = await getDocs(q);
-				if (!eventsSnapshot.empty) {
-					const lastVisible =
-						eventsSnapshot.docs[eventsSnapshot.docs.length - 1];
-					// Saves the last document snapshot in the map
-					setPageEnds((prevPageEnds) =>
-						new Map(prevPageEnds).set(page, lastVisible)
+					// The ?. operator short-circuits and returns undefined if pageEnds is undefined or null, preventing a TypeError from being thrown
+					// For subsequent pages, fetch the pageEnds from paginationSnapshots
+					const pageEndDocRef = doc(
+						db,
+						"paginationSnapshots",
+						`page${page - 1}`
 					);
-					const eventsData: Event[] = [];
-					eventsSnapshot.forEach((doc) => {
-						const eventData: Event = doc.data() as Event;
-						eventsData.push(eventData);
-					});
-					setEvents(eventsData);
-				} else {
-					setIsEmpty(true);
+					const pageEndsSnapshot = await getDoc(pageEndDocRef);
+					// If there are no pageEnds, set isEmpty to true
+					if (!pageEndsSnapshot.exists) {
+						setIsEmpty(true);
+						setLoading(false);
+						return;
+					}
+					const pageEnds = pageEndsSnapshot.data();
+					eventQuery = queryF(
+						eventsRef,
+						orderBy("dateTime"),
+						startAfter(pageEnds?.endDocId),
+						limit(20)
+					);
 				}
-				setLoading(false);
+
+				const eventsSnapshot = await getDocs(eventQuery);
+				const eventsData: Event[] = [];
+				eventsSnapshot.forEach((doc) => {
+					const eventData: Event = doc.data() as Event;
+					eventsData.push(eventData);
+				});
+				setEvents(eventsData);
+				setIsEmpty(eventsData.length === 0);
 			} catch (error) {
 				console.log(error);
+			} finally {
+				setLoading(false);
 			}
 		};
 		fetchEvents();
-		// Scrolls to the top of the page once the page changes 
+		// Scrolls to the top of the page once the page changes
 		window.scrollTo(0, 0);
 	}, [page]);
 
---
+++ my-react-frontend/src/events.ts
@@ -0,0 +1,5 @@
+async function getEvents(params: type) {}
+
+async function get(params: type) {}
+
+async function getEventCounts(params: type) {}
