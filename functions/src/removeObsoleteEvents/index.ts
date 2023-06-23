da54cea018e2e0bde459d0c5231372ce12ea78d5
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-06-23 12:25:28 +0200

    - renamed cleanupEvents function to cleanupOldEvents to make it clearer - added functions to remove obselete events which are no longer found by the scraper

---
+++ functions/src/removeObsoleteEvents/index.ts
@@ -0,0 +1,31 @@
+import Event from "../types/Event";
+import { adminDb } from "../firebaseAdmin";
+// removes events which are no longer found by the scrapers possible due to the organizer changing certain parameters which
+export async function removeObsoleteEvents(
+	platform: string,
+	scrapedEvents: Event[]
+) {
+	try {
+		// Retrieve all existing events for the relevant event platform from Firebase
+		const eventsRef = adminDb.collection("events");
+		const snapshot = await eventsRef
+			.where("eventPlatform", "==", platform)
+			.get();
+		snapshot.forEach(async (docu: any) => {
+			const event = docu.data();
+			// gets the unique ID to compare with
+			const eventId = event.id;
+			// check if this event is in the scrapedEvents array
+			const existsInscraped = scrapedEvents.some(
+				(scrapedEvent) => scrapedEvent.id === eventId
+			);
+
+			// if not in scrapedEvents, remove from Firestore
+			if (!existsInscraped) {
+				await adminDb.collection("events").doc(docu.id).delete();
+			}
+		});
+	} catch (error) {
+		console.error("Error removing obselete events: ", error);
+	}
+}
