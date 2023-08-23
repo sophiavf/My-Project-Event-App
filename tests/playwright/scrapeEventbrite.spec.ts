53855314fe000c9e1d109abc6cccd46eb1a906af
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-08-23 13:51:12 +0200

    refactored Eventbrite playwright tests to reuse events array and improved way the pages object is instantiated and used in the tests to be passed as a function param

--- tests/playwright/scrapeEventbrite.spec.ts
+++ tests/playwright/scrapeEventbrite.spec.ts
@@ -3,49 +3,81 @@ import {
 	goToNextPage,
 	processEvents,
 } from "../../functions/src/my-scraper/scrapers/ScrapeEventbrite";
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import playwright, { Page, BrowserContext, Browser } from "playwright-core";
 import { eventbriteUrl } from "../../functions/src/index";
 
+// Annotate entire file as serial.
+test.describe.configure({ mode: 'serial' });
+
+//Import interfaces
+import Event from "../../functions/src/types/Event";
+
+//import test json data
 import { readFileSync } from "fs";
 import { join } from "path";
+import { Timestamp } from "@google-cloud/firestore";
 const dataFilePath = join(__dirname, "./eventbriteTestData.json");
 const rawData = readFileSync(dataFilePath, "utf-8");
 const data = JSON.parse(rawData);
 
-test("page is opened", async ({ page }) => {
+// shared test variables
+let browser: Browser, context: BrowserContext, page: Page;
+
+let events: Event[];
+
+test.beforeAll(async () => {
+	browser = await playwright.chromium.launch();
+});
+
+test.afterAll(async () => {
+	await browser.close();
+});
+
+test.beforeEach(async () => {
+	context = await browser.newContext();
+	page = await context.newPage();
+});
+
+test.afterEach(async () => {
+	await context.close();
+});
+
+test("EventBrite page is opened", async () => {
 	await page.goto(eventbriteUrl);
 	expect(await page.url()).toBe(eventbriteUrl);
 });
 
-test("scrapeEventbrite returns an array", async ({ page }) => {
-	const events = await scrapeEventbrite(page, eventbriteUrl);
+test("scrapeEventbrite returns an array containing atleast 1 event", async () => {
+	events = await scrapeEventbrite(page, eventbriteUrl);
 	console.log(events);
 	expect(Array.isArray(events)).toBe(true);
-});
-
-test("goToNextpage returns a boolean", async ({ page }) => {
-	const hasNextpage = await goToNextPage(page);
-	expect(typeof hasNextpage).toBe("boolean");
+	expect(events.length).toBeGreaterThan(0);
 });
 
 test("processEvents returns an array", () => {
 	console.log(typeof data);
-
 	const processedEvents = processEvents(data);
 	expect(Array.isArray(processedEvents)).toBe(true);
 });
 // Test for the scrapeEventbrite function
-test("scrapeEventbrite should return valid Event objects", async ({ page }) => {
-	const events = await scrapeEventbrite(page, eventbriteUrl);
+test("scrapeEventbrite should return valid Event objects", async () => {
+	console.log(events);
 	for (const event of events) {
-		// Add your validations here, for example:
 		expect(event).toHaveProperty("id");
 		expect(event).toHaveProperty("name");
 	}
 });
 
+test("scrapeEventbrite returns an array with no duplicates", async () => {
+	const ids = events.map((event) => event.id);
+	const idsSet = new Set(ids);
+	//This test will fail if there are any duplicate ids in the events array, as the size of the ids array will be larger than the size of the idsSet.
+	expect(ids.length).toBe(idsSet.size);
+});
+
 // Test for the goToNextpage function
-test("goToNextpage should navigate to the next page", async ({ page }) => {
+test("goToNextpage should navigate to the next page", async () => {
 	await page.goto(eventbriteUrl);
 	const initialUrl = page.url();
 	const hasNextpage = await goToNextPage(page);
@@ -56,14 +88,8 @@ test("goToNextpage should navigate to the next page", async ({ page }) => {
 	expect(initialUrl).not.toBe(newUrl);
 });
 
-test("scrapeEventbrite returns an array with no duplicates", async ({
-	page,
-}) => {
-	const events = await scrapeEventbrite(page, eventbriteUrl);
-
-    const ids = events.map(event => event.id);
-	const idsSet = new Set(ids);
-
-	//This test will fail if there are any duplicate ids in the events array, as the size of the ids array will be larger than the size of the idsSet.
-	expect(ids.length).toBe(idsSet.size);;
+test("goToNextpage returns a boolean", async () => {
+	const hasNextpage = await goToNextPage(page);
+	expect(typeof hasNextpage).toBe("boolean");
+	expect(hasNextpage).toBe(true);
 });
